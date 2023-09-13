38327aa09c9a6caf96d942fe804d48d2c08e9d56
morispolanco <morispolanco@gmail.com>
2023-09-13 07:56:07 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,34 @@
 import openai
 import streamlit as st
 
-with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+def assistant():
+    with st.sidebar:
+        openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+        "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+        "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+        "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-st.title("💬 Chatbot")
+    st.title("💬 Chatbot")
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = []
 
-for msg in st.session_state.messages:
-    if msg["role"] == "assistant":
-        st.chat_message(msg["role"]).write(msg["content"])
-
-prompt = "Your task is to optimize prompts by analyzing the original prompts and creating new prompts that clearly communicate the desired outcome and provide specific guidelines for the type of response required. Your optimized prompts should encourage creativity, originality, and accuracy in the generated responses. Please focus on creating prompts that are clear, concise, and specific to the task at hand."
-
-if prompt:
     if not openai_api_key:
         st.info("Please add your OpenAI API key to continue.")
         st.stop()
 
     openai.api_key = openai_api_key
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    msg = response.choices[0].message
-    st.session_state.messages.append(msg)
-    st.chat_message("assistant").write(msg.content)
+
+    prompt = "Your task is to optimize prompts by analyzing the original prompts and creating new prompts that clearly communicate the desired outcome and provide specific guidelines for the type of response required. Your optimized prompts should encourage creativity, originality, and accuracy in the generated responses. Please focus on creating prompts that are clear, concise, and specific to the task at hand."
+
+    if prompt:
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+    user_input = st.text_input("User Input")
+
+    if user_input:
+        st.session_state.messages.append({"role": "user", "content": user_input})
+        response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+        msg = response.choices[0].message
+        st.session_state.messages.append(msg)
+        st.chat_message("assistant").write(msg.content)
