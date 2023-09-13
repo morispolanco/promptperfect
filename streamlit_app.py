2b7a356726e30ff638170b35eb72e0ffb4bacdf5
morispolanco <morispolanco@gmail.com>
2023-09-13 07:49:59 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,31 @@
-import streamlit as st
 import openai
+import streamlit as st
 
-# Configurar la clave de la API de OpenAI
-api_key = st.sidebar.text_input("Enter your OpenAI API key", type="password")
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-if not api_key:
-    st.warning("Please enter a valid API key to continue.")
-else:
-    openai.api_key = api_key
-    # Continuar con el resto del código que utiliza la clave de API
+st.title("💬 Chatbot")
 
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
 
-# Crear la interfaz de usuario
-st.title("Mejorador de Prompts")
-prompt = st.text_area("Ingresa el prompt:", value="Your task is to optimize prompts by analyzing the original prompts and creating new prompts that clearly communicate the desired outcome and provide specific guidelines for the type of response required. Your optimized prompts should encourage creativity, originality, and accuracy in the generated responses. Please focus on creating prompts that are clear, concise, and specific to the task at hand.")
-if st.button("Generar Mejora"):
-    # Generar la mejora del prompt
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=500
-    )
-    improved_prompt = response.choices[0].text.strip()
+prompt = "Your task is to optimize prompts by analyzing the original prompts and creating new prompts that clearly communicate the desired outcome and provide specific guidelines for the type of response required. Your optimized prompts should encourage creativity, originality, and accuracy in the generated responses. Please focus on creating prompts that are clear, concise, and specific to the task at hand."
 
-    # Mostrar la mejora del prompt
-    st.write("Mejora del prompt:")
-    st.write(improved_prompt)
+if prompt:
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    openai.api_key = openai_api_key
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message
+    st.session_state.messages.append(msg)
+    st.chat_message("assistant").write(msg.content)
