e06df01a2b7898f3a87339127d69ff5559034824
morispolanco <morispolanco@gmail.com>
2023-09-13 08:09:24 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,8 @@ def assistant():
     with st.sidebar:
         openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
         "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-        "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-        "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
-
-    st.title("💬 Chatbot")
+        
+    st.title("💬 Mejorador de prompts")
 
     if "messages" not in st.session_state:
         st.session_state["messages"] = []
